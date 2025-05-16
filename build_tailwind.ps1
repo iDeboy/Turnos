74516dd31a811dec11f03241425e9463b87be742
iDeboy <honorio.acosta.ruiz@outlook.com>
2025-05-15 21:44:29 -0600

    tailwind build

---
+++ build_tailwind.ps1
@@ -0,0 +1,24 @@
+$files = Get-Content "tailwind-files.txt"
+$tailwind = "C:\tailwindcss\tailwindcss.exe" <# Tailwind command #>
+
+foreach ($line in $files) {
+    $parts = $line -split "\|"
+    $i = $parts[0]
+    $o = $parts[1]
+
+    Write-Host "Input: $i | Output: $o"
+
+    if (!(Test-Path $i)) {
+        Write-Host "El archivo de entrada $i no existe."
+        continue
+    }
+
+    & $tailwind -i $i -o $o --minify *>$null 2>$null
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "No se genero el archivo $o."
+    } else {
+        Write-Host "Compilación exitosa: $o"
+    }
+
+}
